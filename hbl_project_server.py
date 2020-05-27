54b506b25dde86061939d59ee505a35a7a0e3746
BrandonTang89 <tangyuhanbrandon@gmail.com>
2020-05-27 17:00:17 +0800

    Updated Personal Upate Page with Better CSS

--- hbl_project_server.py
+++ hbl_project_server.py
@@ -312,6 +312,6 @@ def update_personal_links(index_number, class_name):
 
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port='3000', debug=True)
+    # app.run(host='0.0.0.0', port='3000', debug=True)
     from waitress import serve
     serve(app, host='0.0.0.0', port=8080)
